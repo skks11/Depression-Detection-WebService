020e02c441280d3fe7aa506ca2971a43bbcf5d3e
zqr515 <61657982+zqr515@users.noreply.github.com>
2021-10-18 17:15:53 +0800

    Update single_model.py

--- src/single_model.py
+++ src/single_model.py
@@ -1,30 +1,18 @@
+import contractions
 import numpy as np
 import pandas as pd
 import os
-import matplotlib.pyplot as plt
 import re
-import inline
+from sentence_transformers import SentenceTransformer
 
 import nltk
 from nltk.corpus import stopwords
-from nltk import PorterStemmer
 
 import preprocessor as p
 
-from gensim.models import KeyedVectors
-
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.layers import Dense, Input, LSTM, Embedding, Dropout, Activation
-from keras.layers import Bidirectional, GlobalMaxPool1D
-from keras.models import Model
-
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.cluster import KMeans
-from sklearn.svm import SVC
+
 from sklearn.metrics import accuracy_score, classification_report
-from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.linear_model import SGDClassifier
@@ -42,14 +30,20 @@ class TextModel(object):
         self.c_re = re.compile('(%s)' % '|'.join(self.contractions.keys()))
         self.BAD_SYMBOLS_RE = re.compile('[^0-9a-z #+_]')
         self.X = []  # X-- clean dataset
-        self.sgd = Pipeline([('vect', CountVectorizer()),
-                             ('tfidf', TfidfTransformer()),
-                             ('clf',
-                              SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, random_state=42, max_iter=5,
-                                            tol=None)),
-                             ])
 
-        # model configs here
+        self.sgd = SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, random_state=42, max_iter=5,
+                                 tol=None)
+
+    def transformers(self, text):
+        self.model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+        # self.dataset["content"] = [float(str(i).replace(",", "")) for i in self.dataset["content"]]
+        text = self.dataset['content'].tolist()
+        des_embeddings = []
+        for i, des in enumerate(text):
+            des_embeddings.append(self.model.encode(des))
+        # des_embeddings = des_embeddings.dropna()
+        # print(des_embeddings)
+        return des_embeddings
 
     def expandContractions(self, text):
         def replace(match):
@@ -79,11 +73,14 @@ class TextModel(object):
 
             cleaned_dataset.append(text)
             self.X = cleaned_dataset
+
         # return cleaned_dataset
 
     def train(self):
+        self.X = self.transformers(self.X)
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.X, self.dataset.label,
                                                                                 test_size=0.3, random_state=42)
+        # print(self.x_train)
         self.sgd.fit(self.x_train, self.y_train)
 
     def pred(self):
@@ -91,7 +88,9 @@ class TextModel(object):
         print('accuracy %s' % accuracy_score(y_pred, self.y_test))
         print(classification_report(self.y_test, y_pred, digits=5))
 
-text_model = TextModel('./data/processed_data/final.csv', './data/processed_data/contractions.json')
+
+text_model = TextModel('./input/final.csv', './input/contractions.json')
 text_model.clean()
+text_model.train()
 text_model.train()
 text_model.pred()
